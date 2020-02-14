b600f0a30c31624bddaadf5c898a6294bb41d2f8
github TakumiFuzawa <futaku0811@gmail.com>
2020-02-14 19:17:33 +0900

    Initial Commit

---
+++ collection_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collection_test
+//
+//  Created by Takumi Fuzawa on 2020/02/14.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
